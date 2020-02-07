5ebb99fe998b09146314a652be369fc459062ddc
叶晓兵 <865756739@qq.com>
2020-02-07 20:19:09 +0800

    strings print

---
+++ go/strings/print/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+    const sample = "\xbd\xb2\x3d\xbc\x20\xe2\x00\x8c\x98"
+
+    fmt.Println("Println:")
+    fmt.Println(sample)
+
+    fmt.Println("Byte loop:")
+    for i := 0; i < len(sample); i++ {
+        fmt.Printf("%x ", sample[i])
+    }
+    fmt.Printf("\n")
+
+    fmt.Println("Printf with %x:")
+    fmt.Printf("%x\n", sample)
+
+    fmt.Println("Printf with % x:")
+    fmt.Printf("% x\n", sample)
+
+    fmt.Println("Printf with %q:")
+    fmt.Printf("%q\n", sample)
+
+    fmt.Println("Printf with %+q:")
+    fmt.Printf("%+q\n", sample)
+}
