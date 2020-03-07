77090f70e2bb2b936ffb362a492ca0cc55bde6fd
叶晓兵 <865756739@qq.com>
2020-03-07 14:05:01 +0800

    slice

--- go/slice/main.go
+++ go/slice/main.go
@@ -6,8 +6,11 @@ import (
 
 func main() {
     s1 := []byte("hello")
-    fmt.Printf("%s len=%d\n", string(s1), len(s1))
+    fmt.Printf("s:%s len=%d\n", string(s1), len(s1))
 
-    s2 := s1[:len(s1)+1]
-    fmt.Printf("%s len=%d\n", string(s2), len(s2))
+    //s2 := s1[:len(s1)+1]
+    //fmt.Printf("s:%s len=%d\n", string(s2), len(s2))
+
+    s3 := s1[len(s1):]
+    fmt.Printf("s:%s len=%d\n", string(s3), len(s3))
 }
