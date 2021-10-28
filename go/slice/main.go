4e3959eaf27b4d8a86ed687f97fd479274edca0e
Yellia <865756739@qq.com>
2021-10-28 11:40:43 +0800

    增加slice 指针测试功能

--- go/slice/main.go
+++ go/slice/main.go
@@ -4,6 +4,14 @@ import (
     "fmt"
 )
 
+func out(p *[]int) {
+    *p = make([]int,2)
+    p1 := *p
+    fmt.Printf("p1:%p\n", p1)
+
+    p1[0] = 10
+}
+
 func main() {
     s1 := []byte("hello")
     fmt.Printf("s:%s len=%d\n", string(s1), len(s1))
@@ -13,4 +21,8 @@ func main() {
 
     s3 := s1[len(s1):]
     fmt.Printf("s:%s len=%d\n", string(s3), len(s3))
+
+    var p []int
+    out(&p)
+    fmt.Printf("p:%p, %v\n", p, p)
 }
