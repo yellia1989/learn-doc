1b11cfd6e56e12110db5a437f1b7eca57cde70cb
root <Yellia>
2022-05-28 14:45:14 +0800

    1

--- 代码/scala/2.HelloWorld/HelloWorld.scala
+++ 代码/scala/2.HelloWorld/HelloWorld.scala
@@ -1,3 +1,18 @@
 object HelloWorld extends App {
   println("Hello World")
+
+  var p = "111"
+  val p2 = "111"
+  println(p, p2)
+
+  p = "222"
+  //val类型的变量不能赋值
+  //p2 = "222"
+  println(p, p2)
+
+  def func1(s:String) {
+    println(s) 
+  }
+
+  func1("func1")
 }
